ecb74609dca616c7a2378b8cb5481818e70413b7
CHIGODI <chiegody254@gmail.com>
2024-04-23 00:16:23 +0300

    Add task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+""" employee to do to json """
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    employee_name = ''
+    json_data = {}
+    list_1 = []
+
+    for user in users.json():
+        if user.get('id') == int(sys.argv[1]):
+            employee_name = user.get('username')
+    for todo in todos.json():
+        if todo.get('userId') == int(sys.argv[1]):
+            list_1.append({
+               "task": todo.get("title"),
+               "completed": todo.get("completed"),
+               "username": employee_name
+            })
+    json_data.update({sys.argv[1]: list_1})
+
+    filename = f'{sys.argv[1]}.json'
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(json_data, file)
