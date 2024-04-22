db4940a181d113ce43bb1e19f97f6da0d3240020
CHIGODI <chiegody254@gmail.com>
2024-04-22 22:59:06 +0300

    Add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" employee to do to csv """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    employee_name = ''
+    csv = []
+
+    for user in users.json():
+        if user.get('id') == int(sys.argv[1]):
+            employee_name = user.get('name')
+    for todo in todos.json():
+        if todo.get('userId') == int(sys.argv[1]):
+            csv.append(f'{sys.argv[1]}, {employee_name},
+                       {todo.get("completed")}, {todo.get("title")}')
+
+    filename = f'{sys.argv[1]}.csv'
+    with open(filename, mode='w', newline='') as file:
+        for row in csv:
+            file.write(row + '\n')
