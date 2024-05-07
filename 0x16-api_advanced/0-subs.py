4d9680f059895341eeeb921cda8051d14c52fdeb
CHIGODI <chiegody254@gmail.com>
2024-05-07 15:47:36 +0300

    Add if block to check parama

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns number of subs for a subreddit """
-    if subreddit:
-        url = f'https://www.reddit.com/r/{subreddit}/about.json'
-        headers = {'User-Agent': 'Chrome'}
-        response = requests.get(url, headers=headers,  allow_redirects=False)
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Chrome'}
+    response = requests.get(url, headers=headers,  allow_redirects=False)
     try:
         return response.json().get('data').get('subscribers')
     except:
