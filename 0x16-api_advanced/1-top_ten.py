1c833aec367ac8f87f0a13cae47981ba1a582fe0
CHIGODI <chiegody254@gmail.com>
2024-05-07 12:51:00 +0300

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Print top ten hot posts """
+    if subreddit:
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+        headers = {'User-Agent': 'Chrome'}
+        response = requests.get(url, headers=headers,  allow_redirects=False)
+        if response.status_code == 200:
+            posts = response.json().get('data').get('children')
+            for post in posts:
+                print(post.get('data').get('title'))
+        print('None')
